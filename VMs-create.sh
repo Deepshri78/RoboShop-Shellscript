b64c6b6a6fa58b3c35b9774fb7c3d92c9377c8d7
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-21 12:43:14 -0400

    Correction 1

--- VMs-create.sh
+++ VMs-create.sh
@@ -1,7 +1,9 @@
 echo "Creating VMs on Azure using shell script"
 
-catalogue user cart payment shipping 
-user cart payment shipping frontend mongodb redis mysql rabbitmq dispatch
+declare -a vmimage
+
+vmimage = (catalogue user cart payment shipping dispatch mongodb mysql redis rabbitmq)
+
 
 for component in vmimage ; do
 az vm create --resource-group Roboshop-Using-Shell --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name azure-training-2023-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-sku "" --size Standard_B1s --nsg NSG_Roboshop_Using-Shell
