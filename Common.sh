b3514960cf11db489f7f2e6818f097f4a4bd1509
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-29 12:04:11 -0400

    Catalogue issue 1

--- Common.sh
+++ Common.sh
@@ -21,57 +21,58 @@ fi
 
 APPREQ () {
 
- echo " To verify whether Roboshop user already exists or not"
+ echo -e "\e[32m  To verify whether Roboshop user already exists or not"
 
  id roboshop &>>${LOG_FILE}
 
  if [ $? -ne 0 ]; then
- echo " Logged in user is not roboshop, need to add roboshop user. "
+ echo -e "\e[32m  Logged in user is not roboshop, need to add roboshop user. \e[0m"
  useradd roboshop &>>${LOG_FILE}
  StatusCheck $?
  fi
 
- echo " Once roboshop user is added, need to download files for specific ${COMPONENT} "
+ echo -e "\e[32m  Once roboshop user is added, need to download files for specific ${COMPONENT} \e[0m"
 
  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
   
 
   if [ ${COMPONENT} == frontend ]; then
-    echo " Stop NGINX service if already running. "
+    echo -e "\e[32m  Stop NGINX service if already running. \e[0m"
     yum install nginx -y
     #systemctl stop ${COMPONENT}.service &>>${LOG_FILE}
     StatusCheck $?
 
-    echo " Change current directory. "
+    echo -e "\e[32m  Change current directory. \e[0m"
     cd /usr/share/nginx/html &>>${LOG_FILE}
     StatusCheck $?
 
-    echo " Delete older files if any. "
+    echo -e "\e[32m  Delete older files if any. \e[0m"
     rm -rf * &>>${LOG_FILE}
     StatusCheck $?
 
-    echo " Unzip downloaded file in current directory. "
+    echo -e "\e[32m  Unzip downloaded file in current directory. \e[0m"
     unzip /tmp/${COMPONENT}.zip &>>${LOG_FILE}
     StatusCheck $?
 
-    echo " Remove/Rename main folder. "
+    echo -e "\e[32m  Remove/Rename main folder. \e[0m"
     mv ${COMPONENT}-main/static/* . &>>${LOG_FILE}
     StatusCheck $?
 
-    echo " Remove/rename configuration file. "
+    echo -e "\e[32m  Remove/rename configuration file. \e[0m"
     mv ${COMPONENT}-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
     StatusCheck $?
 
-    echo " Start the service. "
+    echo -e "\e[32m  Start the service. \e[0m"
     systemctl start nginx &>>${LOG_FILE}
     StatusCheck $?
 
   else
-    echo " Stop service if already running. "
-    systemctl stop ${COMPONENT}.service &>>${LOG_FILE}
+  if 
+    echo -e "\e[32m  Stop service if already running. \e[0m"
+    #systemctl stop ${COMPONENT}.service &>>${LOG_FILE}
     StatusCheck $?
 
-    echo " Remove files from Home folder before unzipping new files. "
+    echo -e "\e[32m  Remove files from Home folder before unzipping new files. \e[0m"
     cd /home/roboshop/  &>>${LOG_FILE}
     StatusCheck $?
     rm -rf ${COMPONENT} &??${LOG_FILE}
